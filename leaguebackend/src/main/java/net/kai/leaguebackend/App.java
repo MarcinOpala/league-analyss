bedf9392cf4f5e75642913c0146612a9124907e7
Marcin Opala <marcin.opala90@gmail.com>
2019-02-20 22:32:58 +0100

    My Project Setup Completed Successfully

---
+++ leaguebackend/src/main/java/net/kai/leaguebackend/App.java
@@ -0,0 +1,13 @@
+package net.kai.leaguebackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
